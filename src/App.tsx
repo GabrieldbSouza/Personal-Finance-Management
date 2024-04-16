79cd0e6bcb434e4a668f3d6264d1983bfffa0a01
Gabriel <gabrieldebritosouza15@gmail.com>
2024-04-16 14:35:03 -0300

    atualizaçao geral

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import HomePage from './components/HomePage'
+import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/login/LoginPage'
+import UserPage from './pages/user/UserPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RegisterPage from './pages/register/RegisterPage';
 
 function App() {
- return (
-  <HomePage />
- )
- 
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} /> 
+        <Route path='/userpage' element={<UserPage />} />           
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App
+export default App;
