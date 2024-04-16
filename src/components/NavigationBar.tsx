52027348db6306b7662e41ab988139fa675da500
Gabriel <gabrieldebritosouza15@gmail.com>
2024-04-16 14:34:35 -0300

    atuializacao do NavigationBar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,14 +1,27 @@
+import { useNavigate } from 'react-router-dom'
 import '../static/NavigationBar.css' 
 
 function NavigationBar() {
+
+  const navigation = useNavigate();
+  const goToLoginPage = () => {
+    navigation('/login');
+  }
+  const goToRegisterPage = () => {
+    navigation('/register')
+  }
+
+  const goToHomePage = () =>{
+    navigation('/')
+  }
   return (
     <div className='NavigationBar'>
       <div className="Logo">
-        <button id='Logo'>Personal Finance Management</button>
+        <button id='Logo' onClick={goToHomePage}>Personal Finance Management</button>
       </div>
       <div className='BtnAccount'>
-        <button id='Connect'>Connect</button>
-        <button id='Account'>My Account</button> 
+        <button id='Connect' onClick={goToRegisterPage}>Connect</button>
+        <button id='Account' onClick={goToLoginPage}>My Account</button> 
       </div>
     </div>
   )
